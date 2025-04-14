b3f979a0fd81685f7d526908a6315675f60404af
SanaatanX <137610577+SanaatanX@users.noreply.github.com>
2025-04-14 23:29:13 +0800

    Batch commit of 50 files

---
+++ 网站系统/team-pc/.history/src/views/layout/index_20250414213807.vue
@@ -0,0 +1,26 @@
+<template>
+    <el-container class="maincontainer">
+    <el-header class="header">Header</el-header>
+    <el-main class="main">Main</el-main>
+    <el-footer class="footer">Footer</el-footer>
+    </el-container>
+</template>
+
+<script setup lang="ts"></script>
+<style scoped lang="scss">
+.maincontainer {
+    height: 100%;
+    .header {
+        background-color: #1077cc;
+        display: flex;
+        align-items: center;
+        padding-left: ;
+    }
+    .main{
+        background-color: rebeccapurple;
+    }
+    .footer{
+        background-color: coral;
+    }
+}
+</style>
