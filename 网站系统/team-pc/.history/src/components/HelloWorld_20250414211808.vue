600145d248b28e9aa215f22427851f35023dc3e2
SanaatanX <137610577+SanaatanX@users.noreply.github.com>
2025-04-14 23:28:57 +0800

    Batch commit of 50 files

---
+++ 网站系统/team-pc/.history/src/components/HelloWorld_20250414211808.vue
@@ -0,0 +1,31 @@
+<template>
+  <div style="color: red;"> 社团管理系统</div>
+  <el-button type="primary" icon="Plus" size="default" >新增</el-button>
+  <el-icon :size="20">
+    <Edit />
+  </el-icon>
+  <div>
+
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed, ref } from 'vue'
+import { testStore } from '@/store/test';
+import { Edit } from '@element-plus/icons-vue/dist/types';
+//获取Store
+const store = testStore()
+//获取count值
+const count = computed((=> return ))
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+</script>
+
+
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
