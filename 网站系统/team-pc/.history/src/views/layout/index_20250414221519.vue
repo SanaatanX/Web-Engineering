a7087d5e0113dc6c1670d441927fd64788b4c611
SanaatanX <137610577+SanaatanX@users.noreply.github.com>
2025-04-14 23:29:16 +0800

    Batch commit of 50 files

---
+++ 网站系统/team-pc/.history/src/views/layout/index_20250414221519.vue
@@ -0,0 +1,33 @@
+<template>
+    <el-container class="maincontainer">
+    <el-header class="header">
+        <logo></logo>
+        <tabs></tabs>
+    </el-header>
+    <el-main class="main"></el-main>
+    <el-aside class="aside"></el-aside>
+    <el-footer class="footer"></el-footer>
+    </el-container>
+</template>
+
+<script setup lang="ts">
+    import logo from './logo.vue'
+    import tabs from './tabs.vue'
+</script>
+<style scoped lang="scss">
+.maincontainer {
+    height: 100%;
+    .header {
+        background-color: #1077cc;
+        display: flex;
+        align-items: center;
+        padding: 0px;
+    }
+    .main{
+        padding: 0px;
+    }
+    .footer{
+        // background-color: coral;
+    }
+}
+</style>
