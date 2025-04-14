8dc161693b36aae3297cee9a8e754ae0dae182cc
SanaatanX <137610577+SanaatanX@users.noreply.github.com>
2025-04-14 23:29:07 +0800

    Batch commit of 50 files

---
+++ 网站系统/team-pc/.history/src/store/test/index_20250414211158.ts
@@ -0,0 +1,11 @@
+import { count } from 'console'
+import {defineStore} from 'pinia'
+// 第一个参数表示store的唯一id
+export const testStore = defineStore('testStore', {
+    state: () => {
+        return{
+            count: 0
+        }
+    },
+    
+})
